85dd71516f3157c468746855daeee1daaeba4b3c
Clauido <claudio.cortes02@alumnos.ucn.cl>
2023-08-20 18:11:42 -0400

    graph sk and velocity

--- src/streamlit_module.py
+++ src/streamlit_module.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import numpy as np
-from scipy.optimize import curve_fit 
+from scipy.optimize import curve_fit
 
 fileName1 = "UNI_CORR_500_01_modified.txt"
 fileName2 = "UNI_CORR_500_02_modified.txt"
-df1 = pd.read_csv("C:/Users/vicen/OneDrive/Escritorio/Progra científica/Laboratorios/Lab01/src/results/UNI_CORR_500_01_modified.txt", sep="\t")
-df2 = pd.read_csv("C:/Users/vicen/OneDrive/Escritorio/Progra científica/Laboratorios/Lab01/src/results/UNI_CORR_500_02_modified.txt", sep="\t")
+df1 = pd.read_csv("./results/UNI_CORR_500_01_modified.txt", sep="\t")
+df2 = pd.read_csv("./results/UNI_CORR_500_02_modified.txt", sep="\t")
 st.write(f"""
 # {fileName1}       
          """)
@@ -18,7 +18,8 @@ st.write(f"""
          """)
 st.table(df2.head(5))
 
-def show_box_plot(df,fileName):
+
+def show_box_plot(df, fileName):
     random_ids = random.sample(list(df["PersID"]), 10)
     id_filter = df["PersID"].isin(random_ids)
     df = df.loc[id_filter]
@@ -28,17 +29,19 @@ def show_box_plot(df,fileName):
     plt.xlabel("PersID")
     plt.ylabel("Velocity")
     plt.title("Velocity by Frame ({})".format(fileName))
-    #plt.savefig("C:/Users/vicen/OneDrive/Escritorio/Progra científica/Laboratorios/Lab01/images/velocity_boxplot.png")
+    # plt.savefig("C:/Users/vicen/OneDrive/Escritorio/Progra científica/Laboratorios/Lab01/images/velocity_boxplot.png")
     st.pyplot(plt)
+
 
 def show_histogram(df):
     with st.sidebar:
         st.write("Opciones")
-        div = st.slider("Número de bins (histograma):",20,250,20)
-    plt.hist2d(x=df["Frame"],y=df["Velocity"],bins=div)
+        div = st.slider("Número de bins (histograma):", 20, 250, 20)
+    plt.hist2d(x=df["Frame"], y=df["Velocity"], bins=div)
     plt.xlabel("Frame")
     plt.ylabel("Velocity")
     st.pyplot(plt)
+
 
 def generate_frame_points(data):
     unique_frames = data['Frame'].unique()
@@ -49,75 +52,85 @@ def generate_frame_points(data):
         frame_points.append((frame_number, frame_data[['X', 'Y']].values))
 
     return frame_points
-   
+
+
 def show_by_frame(data):
     fig, ax = plt.subplots()
     ax.set_xlim(100, 500)
     ax.set_ylim(0, 500)
-    
+
     x_ticks = np.arange(100, 501, 50)
     y_ticks = np.arange(0, 501, 50)
-    
+
     ax.set_xticks(x_ticks)
     ax.set_yticks(y_ticks)
-    
+
     frame_points = generate_frame_points(data)
 
     for frame_number, points in frame_points:
         ax.clear()
         ax.set_title(f"Frame: {frame_number}")
-        
+
         ax.scatter(points[:, 0], points[:, 1], c='blue', label='Coordenada')
-        
+
         ax.set_xticks(x_ticks)
         ax.set_yticks(y_ticks)
-        
+
         st.pyplot(fig)
-        
+
         plt.pause(1/25)
+
 
 def visualize_data_frames(data1, data2):
     with st.sidebar:
         st.write("Opciones")
-        div = st.slider("Número de bins:",0,130,25)
+        div = st.slider("Número de bins:", 0, 130, 25)
     fig, axes = plt.subplots(1, 2, figsize=(15, 6))
-    
+
     # Histograma para el primer DataFrame
-    freq_matrix1, x_edges1, y_edges1 = np.histogram2d(data1["X"], data1["Y"], bins=div)
+    freq_matrix1, x_edges1, y_edges1 = np.histogram2d(
+        data1["X"], data1["Y"], bins=div)
     freq_matrix1 = freq_matrix1.transpose()
-    im1 = axes[0].imshow(freq_matrix1, origin="lower", cmap="plasma", extent=[x_edges1[0], x_edges1[-1], y_edges1[0], y_edges1[-1]], interpolation="nearest", aspect="auto")
+    im1 = axes[0].imshow(freq_matrix1, origin="lower", cmap="plasma", extent=[
+                         x_edges1[0], x_edges1[-1], y_edges1[0], y_edges1[-1]], interpolation="nearest", aspect="auto")
     axes[0].set_title("Matriz de Frecuencias - Data 1")
     axes[0].set_xlabel("X")
     axes[0].set_ylabel("Y")
     plt.colorbar(im1, ax=axes[0], label="Frecuencia")
-    
+
     # Histograma para el segundo DataFrame
-    freq_matrix2, x_edges2, y_edges2 = np.histogram2d(data2["X"], data2["Y"], bins=div)
+    freq_matrix2, x_edges2, y_edges2 = np.histogram2d(
+        data2["X"], data2["Y"], bins=div)
     freq_matrix2 = freq_matrix2.transpose()
-    im2 = axes[1].imshow(freq_matrix2, origin="lower", cmap="plasma", extent=[x_edges2[0], x_edges2[-1], y_edges2[0], y_edges2[-1]], interpolation="nearest", aspect="auto")
+    im2 = axes[1].imshow(freq_matrix2, origin="lower", cmap="plasma", extent=[
+                         x_edges2[0], x_edges2[-1], y_edges2[0], y_edges2[-1]], interpolation="nearest", aspect="auto")
     axes[1].set_title("Matriz de Frecuencias - Data 2")
     axes[1].set_xlabel("X")
     axes[1].set_ylabel("Y")
     plt.colorbar(im2, ax=axes[1], label="Frecuencia")
     plt.tight_layout()
-    #plt.savefig("images/" + "double_hist2d_main2.png")
+    # plt.savefig("images/" + "double_hist2d_main2.png")
     st.pyplot(plt)
+
 
 def visualize_histogram(data):
     with st.sidebar:
         st.write("Opciones")
         div = st.slider("Número de bins:", 20, 200, 10)
     fig, ax = plt.subplots(figsize=(8, 6))
-    
-    freq_matrix, x_edges, y_edges = np.histogram2d(data["X"], data["Y"], bins=div)
+
+    freq_matrix, x_edges, y_edges = np.histogram2d(
+        data["X"], data["Y"], bins=div)
     freq_matrix = freq_matrix.transpose()
-    im = ax.imshow(freq_matrix, origin="lower", cmap="plasma", extent=[x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], interpolation="nearest", aspect="auto")
+    im = ax.imshow(freq_matrix, origin="lower", cmap="plasma", extent=[
+                   x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]], interpolation="nearest", aspect="auto")
     ax.set_title("Matriz de Frecuencias")
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     plt.colorbar(im, ax=ax, label="Frecuencia")
     plt.tight_layout()
     st.pyplot(plt)
+
 
 def correlation(df1):
     fig, ax = plt.subplots()
@@ -127,10 +140,12 @@ def correlation(df1):
     ax.set_title("Correlación entre Velocity y Sk_Value")
     st.pyplot(fig)
 
+
 def curve_func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
-def optimize_sk(df,fileName):
+
+def optimize_sk(df, fileName):
     x_data = df["Sk_Value"].values
     y_data = df["Velocity"].values
 
@@ -143,15 +158,21 @@ def optimize_sk(df,fileName):
     df["Velocity_Fitted"] = curve_func(x_data, a_fit, b_fit, c_fit)
 
     plt.scatter(x_data, y_data, label="Datos reales")
-    plt.plot(x_data, df["Velocity_Fitted"], label="Valores ajustados", color="red")
+    plt.plot(x_data, df["Velocity_Fitted"],
+             label="Valores ajustados", color="red")
     plt.xlabel("Sk_Value")
     plt.ylabel("Velocity")
     plt.legend()
     plt.title(f"Ajuste de curva para {fileName}")
     st.pyplot(plt)
 
-show_box_plot(df1,fileName1)
-show_histogram(df1)
-correlation(df1)
-optimize_sk(df1,fileName1)
-#show_by_frame(df1)
+
+def main():
+    show_box_plot(df1, fileName1)
+    show_histogram(df1)
+    correlation(df1)
+    optimize_sk(df1, fileName1)
+
+
+main()
+# show_by_frame(df1)
--- src/velocity_module.py
+++ src/velocity_module.py
@@ -1,138 +1,177 @@
-from math import sqrt 
+from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import psutil,time,random
+import psutil
+import time
+import random
 from scipy.spatial import KDTree
 from scipy.optimize import curve_fit
 
-#6194813
-#146.83.128.60
+# 6194813
+# 146.83.128.60
 random.seed(1)
 
-def get_resource_info(code_to_measure):
-    resources_save_data = get_resource_usage(code_to_measure=code_to_measure)
-    print(f"Tiempo de CPU: {resources_save_data['tiempo_cpu']} segundos")
-    print(f"Uso de memoria virtual: {resources_save_data['memoria_virtual']} MB")
-    print(f"Uso de memoria residente: {resources_save_data['memoria_residente']} MB")
-    print(f"Porcentaje de uso de CPU: {resources_save_data['%_cpu']} %")
+# def get_resource_info(code_to_measure):
+#     resources_save_data = get_resource_usage(code_to_measure=code_to_measure)
+#     print(f"Tiempo de CPU: {resources_save_data['tiempo_cpu']} segundos")
+#     print(f"Uso de memoria virtual: {resources_save_data['memoria_virtual']} MB")
+#     print(f"Uso de memoria residente: {resources_save_data['memoria_residente']} MB")
+#     print(f"Porcentaje de uso de CPU: {resources_save_data['%_cpu']} %")
 
-# Función que devuelve el tiempo de CPU y el uso de memoria para un código dado
-def get_resource_usage(code_to_measure):
-    process = psutil.Process()
-    #get cpu status before running the code
-    cpu_percent = psutil.cpu_percent()
-    start_time = time.time()
-    code_to_measure()
-    end_time = time.time()
-    end_cpu_percent = psutil.cpu_percent() 
-    cpu_percent = end_cpu_percent - cpu_percent
-    cpu_percent = cpu_percent / psutil.cpu_count()
-    
-    return {
-        'tiempo_cpu': end_time - start_time,
-        'memoria_virtual': process.memory_info().vms / (1024 * 1024),  # Convertir a MB
-        'memoria_residente': process.memory_info().rss / (1024 * 1024),  # Convertir a MB
-        '%_cpu': cpu_percent # Porcentaje de uso de CPU
-    }
+# # Función que devuelve el tiempo de CPU y el uso de memoria para un código dado
+# def get_resource_usage(code_to_measure):
+#     process = psutil.Process()
+#     #get cpu status before running the code
+#     cpu_percent = psutil.cpu_percent()
+#     start_time = time.time()
+#     code_to_measure()
+#     end_time = time.time()
+#     end_cpu_percent = psutil.cpu_percent()
+#     cpu_percent = end_cpu_percent - cpu_percent
+#     cpu_percent = cpu_percent / psutil.cpu_count()
 
-def get_distance(x2,x1,y2,y1):
-  return sqrt((x2-x1)**2+(y2-y1)**2)
+#     return {
+#         'tiempo_cpu': end_time - start_time,
+#         'memoria_virtual': process.memory_info().vms / (1024 * 1024),  # Convertir a MB
+#         'memoria_residente': process.memory_info().rss / (1024 * 1024),  # Convertir a MB
+#         '%_cpu': cpu_percent # Porcentaje de uso de CPU
+#     }
 
-def get_velocity(row,df):
+
+def compare_histograms(df1, df2, fileName1, fileName2):
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    axes[0].hist2d(x=df1["Frame"], y=df1["Velocity"],
+                   bins=len(df1["Frame"].unique())//10)
+    axes[0].set_xlabel("Frame")
+    axes[0].set_ylabel("Velocity")
+    axes[0].set_title(fileName1)
+
+    axes[1].hist2d(x=df2["Frame"], y=df2["Velocity"],
+                   bins=len(df2["Frame"].unique())//10)
+    axes[1].set_xlabel("Frame")
+    axes[1].set_ylabel("Velocity")
+    axes[1].set_title(fileName2)
+    plt.savefig("../images/"+"histogram_velocity_comparisson.png")
+    plt.show()
+
+
+def get_distance(x2, x1, y2, y1):
+    return sqrt((x2-x1)**2+(y2-y1)**2)
+
+
+def get_velocity(row, df):
     id = int(row["PersID"])
     frame = int(row["Frame"])
     group_data = df[df["PersID"] == id]
-    
+
     if group_data["Frame"].min() == frame:
         return 0
-    
+
     prev_data = group_data[group_data["Frame"] == frame - 1]
     prev_x = prev_data["X"].iloc[0]
     prev_y = prev_data["Y"].iloc[0]
-    distancia = get_distance(row["X"],prev_x,row["Y"],prev_y)
+    distancia = get_distance(row["X"], prev_x, row["Y"], prev_y)
     return distancia/0.04
-    
+
+
 def read_file(filename):
-    dataFrame = pd.read_csv("src/data/"+filename,skiprows=3,sep="\t")
+    dataFrame = pd.read_csv("./data/"+filename, skiprows=3, sep="\t")
     dataFrame = dataFrame.rename(columns={
         "# PersID": "PersID"
     })
     return dataFrame
 
+
 def read_modified_file(filename):
     modified_filename = filename.rstrip(".txt")+"_modified.txt"
-    dataFrame = pd.read_csv("src/results/"+modified_filename,sep="\t")
+    dataFrame = pd.read_csv("./results/"+modified_filename, sep="\t")
     return dataFrame
-    
-def compare_histograms(df1, df2,fileName1,fileName2):
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    axes[0].hist2d(x=df1["Frame"], y=df1["Velocity"], bins=len(df1["Frame"].unique())//10)
-    axes[0].set_xlabel("Frame")
-    axes[0].set_ylabel("Velocity")
-    axes[0].set_title(fileName1)
-    
-    axes[1].hist2d(x=df2["Frame"], y=df2["Velocity"], bins=len(df2["Frame"].unique())//10)
-    axes[1].set_xlabel("Frame")
-    axes[1].set_ylabel("Velocity")
-    axes[1].set_title(fileName2)
-    plt.savefig("images/"+"histogram_velocity_comparisson.png")
-    plt.show()
+
 
 def process_data(fileName):
     dataFrame = read_file(fileName)
     dataFrame["Velocity"] = dataFrame.apply(get_velocity, df=dataFrame, axis=1)
-    dataFrame.to_csv("src/results/"+fileName.rstrip(".txt")+"_modified.txt",index=False,sep="\t")
+    dataFrame.to_csv("./results/"+fileName.rstrip(".txt") +
+                     "_modified.txt", index=False, sep="\t")
     return dataFrame
-    
-def calculate_sk(fileName, dataframe, k):
-    dataframe["Sk_Value"] = 0
 
-    for i in dataframe["Frame"].unique():
-        df = dataframe[dataframe["Frame"] == i].copy()
+
+def calculate_sk(fileName, dataframe, k):
+    # Crear una copia del DataFrame para las modificaciones
+    updated_dataframe = dataframe.copy()
+    updated_dataframe["Sk_Value"] = 0
+
+    for i in updated_dataframe["Frame"].unique():
+        df = updated_dataframe[updated_dataframe["Frame"] == i].copy()
         coordenates = df[["X", "Y"]].values
         df["indexes"] = np.arange(len(df))
+
+        tree = KDTree(coordenates)
 
         for id in df["indexes"]:
             query_point = coordenates[id]
 
-            tree = KDTree(coordenates)
             neighborsIndex = tree.query_ball_point(query_point, k)
             neighborsIndex = [index for index in neighborsIndex if index != id]
             neighborsFrame = coordenates[neighborsIndex]
 
             mean_sk = 0
             if len(neighborsIndex) > 0:
-                distances = np.linalg.norm(neighborsFrame - query_point, axis=1)
+                distances = np.linalg.norm(
+                    neighborsFrame - query_point, axis=1)
                 mean_sk = np.sum(distances) / len(neighborsIndex)
 
-            filtered_index = df.index[df["indexes"] == id].values
-            dataframe.at[int(filtered_index), "Sk_Value"] = mean_sk
+            filtered_index = df[df["indexes"] == id].index
+            if len(filtered_index) > 0:
+                index_to_update = filtered_index[0]
+                updated_dataframe.loc[index_to_update, "Sk_Value"] = mean_sk
 
-    dataframe.to_csv("src/results/" + fileName.rstrip(".txt") + "_modified.txt", index=False, sep="\t")
-    return dataframe
-    
+    output_filename = "./data/" + fileName.rstrip(".txt") + "_modified.txt"
+    updated_dataframe.to_csv(output_filename, index=False, sep="\t")
+    return updated_dataframe
+
+
+def show_sk_vel_graph(df, id_person):
+
+    df = df[df["PersID"] == id_person]
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.scatter(df["Velocity"], df["Sk_Value"], marker='^')
+    ax.set_xlabel("Velocity")
+    ax.set_ylabel("Sk_Value")
+    ax.set_title("Correlación entre Velocity y Sk_Value")
+    plt.savefig("../images/"+"sk_vel_graph.png")
+    plt.show()
+
+
 def main():
     files = {
         # Right to Left
-        1:"UNI_CORR_500_01.txt", 
-        2:"UNI_CORR_500_02.txt", 
-        4:"UNI_CORR_500_04.txt", 
-        6:"UNI_CORR_500_06.txt", 
-        8:"UNI_CORR_500_08.txt",
+        1: "UNI_CORR_500_01.txt",
+        2: "UNI_CORR_500_02.txt",
+        4: "UNI_CORR_500_04.txt",
+        6: "UNI_CORR_500_06.txt",
+        8: "UNI_CORR_500_08.txt",
         # Left to Right
-        3:"UNI_CORR_500_03.txt",
-        5:"UNI_CORR_500_05.txt",
-        7:"UNI_CORR_500_07.txt",
-        9:"UNI_CORR_500_09.txt"
+        3: "UNI_CORR_500_03.txt",
+        5: "UNI_CORR_500_05.txt",
+        7: "UNI_CORR_500_07.txt",
+        9: "UNI_CORR_500_09.txt"
     }
-    
+
     fileName1 = files[1]
-    fileName2 = files[5]
-    
-    dataFrame1 = read_modified_file(fileName1)
-    dataFrame2 = read_modified_file(fileName2)
+    fileName2 = files[2]
+    fileName3 = files[3]
+    fileName4 = files[4]
+    fileName5 = files[5]
+    fileName6 = files[6]
+    fileName7 = files[7]
+    fileName8 = files[8]
+    fileName9 = files[9]
 
-    dataFrame1 = calculate_sk(fileName1,dataFrame1,k=5)        
+    id_person = 446
 
-get_resource_info(main)
+    show_sk_vel_graph(read_modified_file(fileName5), id_person)
+
+
+main()
